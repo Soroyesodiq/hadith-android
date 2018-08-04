14f21d55e62ba6b7d2966afe2fe7b87b02c78ebc
ahm507 <ahm507@gmail.com>
2018-08-04 10:04:04 +0200

    Rename refactorings

--- app/src/main/java/org/sonna/www/sonna/BooksTreeService.java
+++ app/src/main/java/org/sonna/www/sonna/BooksTreeService.java
@@ -13,36 +13,36 @@ public class BooksTreeService {
 
 	static final String LOG_TAG = "BooksTreeService";
 
-	private SQLiteDatabase mDb;
-	private SQLiteInstaller mDbHelper;
+	private SQLiteDatabase db;
+	private SQLiteInstaller sqLiteInstaller;
 
 	BooksTreeService(Context context) {
-		mDbHelper = new SQLiteInstaller(context);
+		sqLiteInstaller = new SQLiteInstaller(context);
 	}
 
 	public BooksTreeService install() throws DatabaseCopyException {
-        mDbHelper.install();
+        sqLiteInstaller.install();
 		return this;
 	}
 
 	public BooksTreeService open() throws SQLException {
-		mDbHelper.openDataBase();
-		mDbHelper.close();
-		mDb = mDbHelper.getReadableDatabase();
+		sqLiteInstaller.openDataBase();
+		sqLiteInstaller.close();
+		db = sqLiteInstaller.getReadableDatabase();
 		return this;
 	}
 
 	public void close() {
-		mDbHelper.close();
+		sqLiteInstaller.close();
 	}
 
-	private ArrayList<BooksTreeNode> selectData(String sql, String args[]) {
-		Cursor cursor = mDb.rawQuery(sql, args);
+	private ArrayList<BooksTreeNode> executeQuery(String sql, String args[]) {
+		Cursor cursor = db.rawQuery(sql, args);
 		ArrayList<BooksTreeNode> out = new ArrayList<>();
 		while (cursor != null && cursor.moveToNext()) {
 			out.add(getBooksTreeNodeObject(cursor));
 		}
-		if(cursor!= null) {
+		if(cursor != null) {
 		    cursor.close();
         }
 		return out;
@@ -58,7 +58,7 @@ public class BooksTreeService {
 		String sql = "SELECT * FROM pages where pages MATCH ?";
 		String params = new Formatter().format("book_code:%s page_id:%s", book_code, page_id).toString();
 		String args[] = new String[]{params};
-		return selectData(sql, args);
+		return executeQuery(sql, args);
 	}
 
 	public ArrayList<BooksTreeNode> findKidNodes(String book_code, String page_id) {
@@ -73,14 +73,14 @@ public class BooksTreeService {
 			args = new String[]{param};
 		}
 
-		return selectData(sql, args);
+		return executeQuery(sql, args);
 	}
 
 	public boolean isLeafNode(String book_code, String page_id) {
 		String sql = "SELECT * FROM pages where pages MATCH ?";
 		String param = new Formatter().format("book_code:%s parent_id:%s", book_code, page_id).toString();
 		String[] args = new String[]{param};
-		Cursor cursor = mDb.rawQuery(sql, args);
+		Cursor cursor = db.rawQuery(sql, args);
 		boolean existKids = (cursor != null && cursor.moveToNext());
 		if(cursor!= null) {
 			cursor.close();
@@ -91,7 +91,7 @@ public class BooksTreeService {
 	public ArrayList<BooksTreeNode> search(String terms, int pageLength, int pageNo) {
 		String sql = "SELECT * FROM pages where pages MATCH ? order by book_code,page_id LIMIT ? OFFSET ? ";
 		String args[] = {terms, String.valueOf(pageLength), String.valueOf((pageNo - 1) * pageLength)};
-		Cursor cursor = mDb.rawQuery(sql, args);
+		Cursor cursor = db.rawQuery(sql, args);
 		ArrayList<BooksTreeNode> out = new ArrayList<>();
 		while (cursor != null && cursor.moveToNext()) {
 			out.add(getBooksTreeNodeObject(cursor));
@@ -106,7 +106,7 @@ public class BooksTreeService {
 		String ftsQuery;
 		String sql;
 		String params[];
-		if ("".equals(book_code)) { //empty
+		if (book_code.isEmpty()) { //empty
 			ftsQuery = queryString;
 			sql = "SELECT count(*) AS total_count FROM pages WHERE page_fts MATCH ?";
 		} else {
@@ -114,7 +114,7 @@ public class BooksTreeService {
 			sql = "SELECT count(*) AS total_count FROM pages WHERE page_fts MATCH ?";
 		}
 		params = new String[]{ftsQuery};
-		Cursor cursor = mDb.rawQuery(sql, params);
+		Cursor cursor = db.rawQuery(sql, params);
 		int count;
 		if (cursor != null && cursor.moveToNext()) {
 			String countString = cursor.getString(0);
