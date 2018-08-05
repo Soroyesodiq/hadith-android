be958e0b700c86b8df4a9e3643bd0a47d3fc8b4f
ahm507 <ahm507@gmail.com>
2018-08-05 12:16:05 +0200

    Replace if with switch

--- app/src/main/java/org/sonna/www/sonna/MainActivity.java
+++ app/src/main/java/org/sonna/www/sonna/MainActivity.java
@@ -124,25 +124,26 @@ public class MainActivity extends AppCompatActivity
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
-		if (id == R.id.nav_home_screen) {
-			historyStack.push(curPageId);
-			displayKids("", "");
-			findViewById(R.id.textViewDisplay).setVisibility(View.GONE);
-			findViewById(R.id.listViewTabweeb).setVisibility(View.VISIBLE);
-			return true;
-		} else if (id == R.id.nav_about_us) {
-			showAboutDialogue();
-			return true;
-		} else if (id == R.id.action_exit) {
-			finish();
-			//Go phone home
-			Intent intent = new Intent(Intent.ACTION_MAIN);
-			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			intent.addCategory(Intent.CATEGORY_HOME);
-			startActivity(intent);
-			return true;
+        switch (id) {
+            case R.id.nav_home_screen:
+                historyStack.push(curPageId);
+                displayKids("", "");
+                findViewById(R.id.textViewDisplay).setVisibility(View.GONE);
+                findViewById(R.id.listViewTabweeb).setVisibility(View.VISIBLE);
+                return true;
+            case R.id.nav_about_us:
+                showAboutDialogue();
+                return true;
+            case R.id.action_exit:
+                finish();
+                //Go phone home
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                startActivity(intent);
+                return true;
 
-		}
+        }
 
 		return super.onOptionsItemSelected(item);
 	}
